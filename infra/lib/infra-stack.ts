e0385d2e109654e4173014504d95120999cd58c8
toandh <toandh.dev@gmail.com>
2024-03-15 21:46:00 +0700

    update docker

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,13 +3,12 @@ import {Construct} from 'constructs';
 import {VpcStack} from './vpc-stack';
 import {RdsStack} from "./rds-stack";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import {InstanceClass} from "aws-cdk-lib/aws-ec2";
 
 export class InfraStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-       const vpcStack = new VpcStack(this, 'VpcQuarkusAppDev');
+        const vpcStack = new VpcStack(this, 'VpcQuarkusAppDev');
         new RdsStack(this, 'RdsStack', {
             dbAllocatedStorage: 20,
             dbInstanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
