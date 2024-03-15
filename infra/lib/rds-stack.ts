81c0415f5bd5a3748952371c2c64b3c8f057eddd
toandh <toandh@vnext.vn>
2024-03-15 17:30:01 +0700

    test ci cd

---
+++ infra/lib/rds-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+
+interface RdsStackProps extends cdk.StackProps {
+    vpc: ec2.Vpc;
+    dbName: string;
+    dbInstanceType: ec2.InstanceType;
+    dbAllocatedStorage: number;
+}
+
+export class RdsStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: RdsStackProps) {
+        super(scope, id, props);
+
+        const dbSecurityGroup = new ec2.SecurityGroup(this, 'DBSecurityGroup', {
+            vpc: props.vpc,
+            allowAllOutbound: true,
+            securityGroupName: 'DBSecurityGroup',
+        });
+
+        dbSecurityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(5432),
+            'Allow PostgreSQL access'
+        );
+
+        const dbInstance = new rds.DatabaseInstance(this, id, {
+            engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_16_2 }),
+            instanceType: props.dbInstanceType,
+            vpc: props.vpc,
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PUBLIC,
+            },
+            securityGroups: [dbSecurityGroup],
+            allocatedStorage: props.dbAllocatedStorage,
+            backupRetention: cdk.Duration.days(7),
+            deletionProtection: false,
+            databaseName: 'testdb2',
+            credentials: rds.Credentials.fromPassword('postgres', new cdk.SecretValue('postgres')),
+            instanceIdentifier: 'ecommerce-free'
+        });
+    }
+}
