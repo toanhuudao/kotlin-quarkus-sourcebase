48302915bef632b44888f5968dcb6c0c6f73e158
toandh <toandh.dev@gmail.com>
2024-03-17 00:00:30 +0700

    final

--- infra/lib/DeveloperRoleStack.ts
+++ infra/lib/DeveloperRoleStack.ts
@@ -8,12 +8,10 @@ export class DeveloperRoleStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // Create an IAM group for developers
         const developerGroup = new iam.Group(this, 'DeveloperGroup', {
             groupName: 'developers',
         });
 
-        // Define the policy statement for viewing the "ecommerce" repository
         const codeCommitPolicyStatement = new iam.PolicyStatement({
             actions: [
                 "codecommit:BatchGetRepositories",
@@ -25,7 +23,6 @@ export class DeveloperRoleStack extends cdk.Stack {
             effect: Effect.ALLOW
         });
 
-        // Define a separate policy statement to allow listing all repositories in the account
         const listRepositoriesPolicyStatement = new iam.PolicyStatement({
             actions: [
                 "codecommit:ListRepositories"
@@ -34,19 +31,16 @@ export class DeveloperRoleStack extends cdk.Stack {
             effect: Effect.ALLOW
         });
 
-        // Define a policy statement to allow creating merge requests for "develop" and "master" branches
         const mergeRequestPolicyStatement = new iam.PolicyStatement({
             actions: [
                 "codecommit:CreatePullRequest",
                 "codecommit:UpdatePullRequestStatus",
-                "codecommit:MergePullRequestByFastForward",
                 "codecommit:GitPush",
             ],
             resources: [`arn:aws:codecommit:${this.region}:${this.account}:ecommerce`],
             effect: Effect.ALLOW
         });
 
-        // Define a policy statement to deny pushing to "develop" and "master" branches
         const denyPushPolicyStatement = new iam.PolicyStatement({
             actions: [
                 "codecommit:GitPush"
@@ -60,7 +54,6 @@ export class DeveloperRoleStack extends cdk.Stack {
                     'codecommit:References': [
                         'refs/heads/develop',
                         'refs/heads/master',
-                        'refs/heads/feat-tset',
                     ],
                 },
             },
@@ -83,7 +76,6 @@ export class DeveloperRoleStack extends cdk.Stack {
             effect: Effect.ALLOW
         });
 
-        // Create a policy and attach it to the group
         const policy = new iam.Policy(this, 'DeveloperPolicy', {
             policyName: 'developer-policy',
             statements: [
